112e3733f0777dbaf0a3059095d2178932c56244
syamsul <syamsyul@localhost.com>
2023-06-12 11:03:02 +0700

    set up middleware

--- middleware/auth_middleware.go
+++ middleware/auth_middleware.go
@@ -12,7 +12,9 @@ func JWTChatMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		authorizationHeader := c.GetHeader("Authorization")
 		tokenStringHeader := strings.Replace(authorizationHeader, "Bearer ", "", -1)
-		_, err := helper.ValidateChatJWT(tokenStringHeader)
+
+		claims, err := helper.ValidateJWT(tokenStringHeader)
+		_, err = helper.ValidateChatJWT(tokenStringHeader)
 		if err != nil {
 			c.JSON(http.StatusForbidden, gin.H{
 				"message": "Forbidden",
@@ -21,6 +23,10 @@ func JWTChatMiddleware() gin.HandlerFunc {
 			c.Abort()
 			return
 		}
+
+		//  test
+		c.Set("username", claims["username"])
+		c.Set("user", claims["user"])
 		c.Next()
 	}
 }
